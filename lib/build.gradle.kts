8df48029cd407230fb8f5e8c5266b0ff1bbdce4a
Timofey Volkov <timofey.volkov@tiger.trade>
2024-02-04 21:01:04 +0100

    remove unnecessary dependencies

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -23,11 +23,7 @@ dependencies {
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
-    // This dependency is exported to consumers, that is to say found on their compile classpath.
-    api(libs.commons.math3)
-
-    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-    implementation(libs.guava)
+    implementation("org.apache.commons:commons-lang3:3.14.0")
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
